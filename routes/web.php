049b31d0a30b89be079e69aaa3ee9b8b8225822f
João Victor Nascimento <victorjoao97@outlook.com>
2019-10-15 20:49:31 -0300

    /home redirect to /admin/home

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home', function () {
-    redirect()->route('admin.home');
+    return redirect()->route('admin.home');
 });
 
 Route::middleware(['auth'])->group(function () {
