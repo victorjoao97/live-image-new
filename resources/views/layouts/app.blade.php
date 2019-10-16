9ad633c5d32c947a7d89595e73bd65e9b165febb
João Victor Nascimento <victorjoao97@outlook.com>
2019-10-16 07:46:33 -0300

    add google button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="qUuz7u6ee4msT9nPqekjraDnuLweAzCxEWc7jLjK22Y" />
 
     <meta name="google-signin-client_id" content="{{env('GOOGLE_CLIENT_ID', false)}}">
     <!-- CSRF Token -->
