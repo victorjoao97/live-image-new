c0db51568faf864db4d4b2fc29987ce864ba7d50
João Victor Nascimento <victorjoao97@outlook.com>
2019-10-26 09:56:24 -0300

    redirect login

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,6 +8,8 @@
     <link rel="icon" href="../../../../favicon.ico">
     <meta name="google-site-verification" content="qUuz7u6ee4msT9nPqekjraDnuLweAzCxEWc7jLjK22Y" />
     <meta name="google-signin-client_id" content="{{env('GOOGLE_CLIENT_ID', false)}}">
+    <script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
+
 
     <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
     <!-- CSRF Token -->
@@ -88,7 +90,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
 
 <!-- Ícones -->
 <script src="https://unpkg.com/feather-icons"></script>
