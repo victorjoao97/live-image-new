ffd3ce038346d9676a5863115605c89b63c49ec5
João Victor Nascimento <victorjoao97@outlook.com>
2019-10-26 10:03:13 -0300

    redirect login

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -102,7 +102,9 @@
     function init() {
         gapi.load('auth2', function() {
             /* Ready. Make a call to gapi.auth2.init or some other API */
-            gapi.auth2.init();
+            gapi.auth2.init({
+                client_id: "{{env('GOOGLE_CLIENT_ID', false)}}"
+            });
         });
     }
     function signOut() {
